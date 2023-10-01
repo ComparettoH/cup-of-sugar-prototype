73ac4b74297cf54ea0e1e7e479f7755052d496bf
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-10-01 17:36:55 -0500

    added submit button to UserFormPage.jsx

--- src/components/UserFormPage/UserFormPage.jsx
+++ src/components/UserFormPage/UserFormPage.jsx
@@ -7,11 +7,31 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 function UserFormPage() {
 
     const history = useHistory();
+    // State variables to store selected values for allergies and dietary restrictions
+    const [selectedAllergy, setSelectedAllergy] = useState('');
+    const [selectedDietaryRestriction, setSelectedDietaryRestriction] = useState('')
+    const [acceptsHomemade, setAcceptsHomemade] = useState(false);
 
     //function that will upload photo to input field or activate in-app camera
     const addUserPic = (event) => {
         event.preventDefault();
     }
+
+    // Function to handle changes in the allergy dropdown
+    const handleAllergyChange = (event) => {
+        setSelectedAllergy(event.target.value);
+    };
+
+    // Function to handle changes in the dietary restriction dropdown
+    const handleDietaryRestrictionChange = (event) => {
+        setSelectedDietaryRestriction(event.target.value);
+    };
+
+    // Function to handle changes in the "Y or N" checkbox
+    const handleAcceptsHomemadeChange = (event) => {
+        setAcceptsHomemade(event.target.checked);
+    };
+
     return (
         <>
             <div>
@@ -31,6 +51,53 @@ function UserFormPage() {
 
                         />
                     </label>
+                </div>
+                <div>
+                    <label htmlFor="allergy">
+                        Select Allergy:
+                        <select id="allergy" value={selectedAllergy} onChange={handleAllergyChange}>
+                            <option value="">None</option>
+                            <option value="nuts">Nuts</option>
+                            <option value="dairy">Dairy</option>
+                            <option value="gluten">Gluten</option>
+                            <option value="shellfish">Shellfish</option>
+                            <option value="soy">Soy</option>
+                            <option value="eggs">Eggs</option>
+                            <option value="other">Other</option>
+                            {/* Add more allergy options as needed */}
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="dietaryRestriction">
+                        Select Dietary Restriction:
+                        <select id="dietaryRestriction" value={selectedDietaryRestriction} onChange={handleDietaryRestrictionChange}>
+                            <option value="">None</option>
+                            <option value="vegetarian">Vegetarian</option>
+                            <option value="vegan">Vegan</option>
+                            <option value="glutenFree">Gluten-Free</option>
+                            <option value="dairyFree">Dairy-Free</option>
+                            <option value="halal">Halal</option>
+                            <option value="kosher">Kosher</option>
+                            <option value="other">Other</option>
+                            {/* Add more dietary restriction options as needed */}
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Accept Homemade Items:
+                        <input
+                            type="checkbox"
+                            checked={acceptsHomemade}
+                            onChange={handleAcceptsHomemadeChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button>
+                        Submit
+                    </button>
                 </div>
             </form>
         </>
