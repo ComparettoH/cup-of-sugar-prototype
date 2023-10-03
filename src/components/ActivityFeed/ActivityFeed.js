8eca43c6884c4324273594ad8ab1ea3e95a3294f
Sam Johnson <s_johnso@yahoo.com>
2023-10-03 18:53:03 -0500

    activity feed page client side/redux start

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -1,25 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import OfferCardContent from '../OfferCardContent/OfferCardContent';
+import RequestCardContent from '../RequestCardContent/RequestCardContent';
+// material ui imports
+import { CardActionArea, Card, Grid } from '@mui/material';
 
 // displays user activity and all offers, requests, and shares
 function ActivityFeed(props) {
     const dispatch = useDispatch();
-    const store = useSelector((store) => store);
-    const [heading, setHeading] = useState('Functional Component');
+    const offers = useSelector((store) => store.offers);
+    const requests = useSelector((store) => store.requests);
 
-    //   gets activity information
+    //   gets activity information for current user's group
     useEffect(() => {
-
         dispatch({ type: 'FETCH_OFFERS' });
         dispatch({ type: 'FETCH_REQUESTS' });
-        dispatch({ type: 'FETCH_SHARES' })
-
     }, []);
 
     return (
-        <div>
-            <h2>{heading}</h2>
-        </div>
+        <Grid container spacing={2}>
+            {/* maps over offers and builds cards for each one */}
+            {offers.map((offer, index) => {
+                // console.log('phrase.public_id', phrase.public_id)
+                // const video = cld.video(phrase.public_id).resize(fill().width(400).height(250));
+                return (
+                    <Grid item key={index} xs={12} sm={6} md={4} >
+
+                        <Card sx={{ width: '100%', }}>
+                            <CardActionArea onClick={() => { }} sx={{}}>
+                                {/* <AdvancedVideo cldVid={video} controls /> */}
+                                <OfferCardContent offer={offer} />
+                            </CardActionArea>
+                            {/* <PhraseCardActions phrase={phrase} /> */}
+                        </Card>
+                    </Grid>
+                );
+            }
+            )}
+             {/* maps over requests and builds cards for each one */}
+            {requests.map((request, index) => {
+                // console.log('phrase.public_id', phrase.public_id)
+                // const video = cld.video(phrase.public_id).resize(fill().width(400).height(250));
+                return (
+                    <Grid item key={index} xs={12} sm={6} md={4} >
+
+                        <Card sx={{ width: '100%', }}>
+                            <CardActionArea onClick={() => { }} sx={{}}>
+                                {/* <AdvancedVideo cldVid={video} controls /> */}
+                                <RequestCardContent request={request} />
+                            </CardActionArea>
+                            {/* <PhraseCardActions phrase={phrase} /> */}
+                        </Card>
+                    </Grid>
+                );
+            }
+            )}
+        </Grid>
     );
 }
 
--- src/redux/sagas/offer.saga.js
+++ src/redux/sagas/offer.saga.js
@@ -3,12 +3,13 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 // This is a worker saga; will be fired upon 'FETCH_USER_PROFILE' actions
 
-function* fetchOffer() {
+function* fetchOffers() {
     try {
         const config = {
             headers: {'Content-Type': 'application/json'},
             withCredentials: true,
         };
+        // gets all offers to be displayed in the activity feed
         const response = yield axios.get('api/offer', config);
         yield put({ type: 'SET_OFFERS', payload: response.data});
     } catch (error) {
@@ -18,7 +19,7 @@ function* fetchOffer() {
 }
 
 function* offerSaga() {
-    yield takeLatest('FETCH_OFFER', fetchOffer);
+    yield takeLatest('FETCH_OFFERS', fetchOffers);
     
 
 };
