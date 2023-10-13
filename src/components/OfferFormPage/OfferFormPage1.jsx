44b74e1231f5d0104bad624bfcdc0eca2f237132
Sam Johnson <s_johnso@yahoo.com>
2023-10-13 07:27:10 -0500

    shows a preview pic of uploaded for profile and object

--- src/components/OfferFormPage/OfferFormPage1.jsx
+++ src/components/OfferFormPage/OfferFormPage1.jsx
@@ -11,6 +11,7 @@ function OfferFormPage1(){
     const history = useHistory();
 
     const [offerImage, setOfferImage] = useState('');
+    const [previewImage, setPreviewImage] = useState('');
     const [itemHeadline, setItemHeadline] = useState('')
 
     const handleOfferFormPage2 = () => {
@@ -23,6 +24,11 @@ function OfferFormPage1(){
         dispatch({ type: 'CREATE_NEW_OFFER1', payload: itemNamePhoto })
         // navigate to page 2 of offer form for this item
         history.push(`/offerform2/${itemHeadline}`)
+    }
+
+    const offerImageUpload = (image) => {
+        setOfferImage(image);
+        setPreviewImage(URL.createObjectURL(image));
     }
 
     return(
@@ -40,11 +46,15 @@ function OfferFormPage1(){
                     sx={{ mb: 2 }}
                     />
                 </label>
+            
                 <div>
                 <h6>Upload an Image here:</h6>
+                {previewImage &&
+                <img src={previewImage}/>
+                }
             </div>
                 <TextField
-                            onChange={e => setOfferImage(e.target.files[0])}
+                            onChange={e => offerImageUpload(e.target.files[0])}
                             type="file"
                             accept="image/*"
                             variant='filled'
