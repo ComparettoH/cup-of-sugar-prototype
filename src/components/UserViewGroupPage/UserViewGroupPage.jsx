56e160280d88e47bad477b60752b28ce8d9f69ca
Gabe Glasco <gabeglasco@gmail.com>
2023-10-16 16:54:35 -0500

    style group page

--- src/components/UserViewGroupPage/UserViewGroupPage.jsx
+++ src/components/UserViewGroupPage/UserViewGroupPage.jsx
@@ -37,9 +37,10 @@ function UserViewGroupPage() {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 250,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
+        width: '70%',
+        bgcolor: 'secondary.main',
+        border: 'info.main',
+        borderWidth: 1,
         boxShadow: 24,
         p: 4,
     };
@@ -75,43 +76,44 @@ function UserViewGroupPage() {
     }
     return (
         <>
-           <Box sx={{ mx: '1rem' }}>
-            <Typography variant='h4' align="center">
-                {group[0]?.group_name}
-            </Typography><br></br>
-            <Typography variant="h5" sx={{fontWeight: 'bold'}}>
-                Your sharing location</Typography>
-               <Typography variant='h6'>
-                {group[0]?.share_location}
+            <Box sx={{ mx: '1rem', mt: 5 }}>
+                <Typography variant='h4' align="center" sx={{ mt: 3, fontWeight: 'bold' }}>
+                    {group[0]?.group_name}
+                </Typography><br></br>
+                <Typography variant="h5" sx={{ mt: 3, fontWeight: 'bold' }}>
+                    Your sharing location</Typography>
+                 <Typography variant='h6'>
+                    {group[0]?.share_location}
                 </Typography>
                 <br></br>
-            <Typography variant="h6" sx={{fontWeight: 'bold'}}>
-                Meet your neighbors </Typography>
-               
-            <FormControl fullWidth={true}>
-                <InputLabel htmlFor="neighbor">Select from neighbors:</InputLabel>
-                <Select
-                    id="neighbor"
-                    value={selectedNeighbor}
-                    input={<OutlinedInput label="Select from neighbors:" />}
-                >
-                    {Array.isArray(groupMembers) && groupMembers.map((member) =>
-                        <MenuItem
+                <Typography variant="h6" sx={{ mt: 3, fontWeight: 'bold' }}>
+                    Meet your neighbors </Typography>
 
-                            key={member.id}
-                            value={member.id}
-                            onClick={() => { handleNeighborSelection(member) }}
-                        >
-                            {member.name}
+                <FormControl fullWidth={true}>
+                    <InputLabel>Select from neighbors:</InputLabel>
+                    <Select
+                        sx={{ mt: 2 }}
+                        id="neighbor"
+                        value={selectedNeighbor}
+                        input={<OutlinedInput label="Select from neighbors:" />}
+                    >
+                        {Array.isArray(groupMembers) && groupMembers.map((member) =>
+                            <MenuItem
 
-                        </MenuItem>
+                                key={member.id}
+                                value={member.id}
+                                onClick={() => { handleNeighborSelection(member) }}
+                            >
+                                {member.name}
 
-                    )}
+                            </MenuItem>
 
-                </Select>
-               
-                <br></br>
-            </FormControl>
+                        )}
+
+                    </Select>
+
+                    <br></br>
+                </FormControl>
             </Box>
 
             <Modal
@@ -122,35 +124,35 @@ function UserViewGroupPage() {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        {selectedMember.name}
-                        <img src={selectedMember.imgpath} alt="Neighbor's profile photo" />
-                    </Typography>
+                    <Typography sx={{ mb: 2 }} id="modal-modal-title" variant="h4" component="h2" align="center">
+                        {selectedMember.name}</Typography>
+                    <img src={selectedMember.imgpath} alt="Neighbor's profile photo" />
+
 
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                         {selectedMember.about}
                     </Typography>
 
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Allergies: {selectedMember && selectedMember.allergy_type
+                    <Typography id="modal-modal-description" sx={{ mt: 2, fontWeight: 'bold' }}>
+                        Allergies:</Typography><Typography>{selectedMember && selectedMember.allergy_type
                             ? removeDuplicates(selectedMember.allergy_type).join(', ')
-                            : 'No allergies'}
-                    </Typography>
+                            : 'No allergies'}</Typography>
 
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Dietary Restrictions: {selectedMember && selectedMember.restriction_type
+
+                    <Typography id="modal-modal-description" sx={{ mt: 2, fontWeight: 'bold' }}>
+                        Dietary Restrictions:</Typography><Typography> {selectedMember && selectedMember.restriction_type
                             ? removeDuplicates(selectedMember.restriction_type).join(', ')
-                            : 'No restrictions'}
-                    </Typography>
+                            : 'No restrictions'}</Typography>
+
                 </Box>
             </Modal>
 
             {user.role > 0 &&
-            <Grid align="center">
-                <Button variant='contained' onClick={() => navAddMember()}>
-                    Add New Member
-                </Button>
-            </Grid>
+                <Grid align="center">
+                    <Button variant='contained' onClick={() => navAddMember()}>
+                        Add New Member
+                    </Button>
+                </Grid>
             }
 
         </>
