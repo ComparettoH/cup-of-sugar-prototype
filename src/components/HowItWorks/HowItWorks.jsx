8af9a4dc72db9198c3824e185e4e6e7255331fc6
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-10-18 16:24:43 -0500

    final touches

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -42,15 +42,15 @@ function HowItWorks({setIsNavVisible}) {
                 <br></br>
                 <section>
                     {/* Squiggle Arrow Pic */}
-                    <img className="cup-of-sugar" src={SquiggleArrow} height={70} width={100} />
+                    <img className="cup-of-sugar" src={SquiggleArrow} height={70} width={70} />
                     <Typography variant="h5" align="center">Offer</Typography>
                     <Typography variant="h6" align="center">Offer food that you have too much of to your neighbors</Typography>
                     {/* Squiggle Arrow Pic */}
-                    <img className="cup-of-sugar" src={SquiggleArrow2} height={70} width={100} />
+                    <img className="cup-of-sugar" src={SquiggleArrow2} height={70} width={70} />
                     <Typography variant="h5" align="center">Request</Typography>
                     <Typography variant="h6" align="center">Request ingredients that you need</Typography>
                     {/* Circle Arrow Pic */}
-                    <img className="cup-of-sugar" src={CircleArrow} height={70} width={100} />
+                    <img className="cup-of-sugar" src={CircleArrow} height={70} width={70} />
                     <Typography variant="h5" align="center">Activity</Typography>
                     <Typography variant="h6" align="center">See how your community is helping each other out!</Typography>
                 </section>
